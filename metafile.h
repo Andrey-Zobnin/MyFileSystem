e44f337ad257fb383d879ed57278d6ff195abdf8
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-03-15 14:45:20 +0300

    update structure to store file metadata

--- metafile.h
+++ metafile.h
@@ -3,11 +3,11 @@
 
 #include <cstring>
 
+// Structure to store file metadata
 struct metafile {
-
-    int count;
-    char name[20];
-    
+    int count; // Number of bytes in data
+    char name[20]; // File name
+    long data_offset; // Offset of data in the file
 };
 
 #endif 
