37a89e0af8797081205c561d91354a31ad940c53
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-03-15 14:36:56 +0300

    add main.cpp two head struct

---
+++ metafile.h
@@ -0,0 +1,13 @@
+#ifndef METAFILE_H
+#define METAFILE_H
+
+#include <cstring>
+
+struct metafile {
+
+    int count;
+    char name[20];
+    
+};
+
+#endif 
---
+++ mfile.h
@@ -0,0 +1,14 @@
+#ifndef MFILE_H
+#define MFILE_H
+
+#include <string>
+#include "metafile.h"
+
+struct mfile {
+    
+    metafile meta;
+    std::string data;
+
+};
+
+#endif 
