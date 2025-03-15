b4e5e2d3c17462cdbb403d0a8f7fe990046274e1
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-03-15 14:56:01 +0300

    refactor struct

--- mfile.h
+++ mfile.h
@@ -5,8 +5,8 @@
 #include "metafile.h"
 
 // Structure to store metadata and file data
-struct mfile {
-    metafile meta; // Metadata
+struct MFile {
+    MetaFile meta; // Metadata
     std::string data; // File data
 };
 
