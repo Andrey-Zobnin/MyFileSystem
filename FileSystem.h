a69c97f8e376b4dc46879b1a7b1267ca84135f3e
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-03-23 10:39:07 +0300

    add new paths and methods for change directory

--- FileSystem.h
+++ FileSystem.h
@@ -3,6 +3,7 @@
 
 #include <fstream>
 #include <string>
+#include <vector>
 #include "mfile.h"
 
 // Class representing a simple file system
@@ -10,6 +11,7 @@ class FileSystem {
 private:
     std::fstream fileStream; // File stream for working with the file system
     long currentOffset; // Current position for writing data
+    std::string currentDirectory; // Current directory path
 
 public:
     // Constructor that opens the specified file
@@ -24,8 +26,11 @@ public:
     // Method to read a file from the file system
     void readFile(const std::string& filename);
 
-    // Method to list all files in the file system
+    // Method to list all files in the current directory
     void listFiles();
+
+    // Method to change the current directory
+    void changeDirectory(const std::string& directory);
 };
 
 #endif 
